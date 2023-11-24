a116111c9700c6dba5cac07442ed8c607735afab
hyunjinb <hyunjinb394@gmail.com>
2023-11-24 16:51:45 +0900

    feat : item 생성

---
+++ back/src/db/models/Item.js
@@ -0,0 +1,43 @@
+import { ItemModel } from "../schemas/item";
+const ObjectId = require('mongoose').Types.ObjectId;
+
+
+class Item {
+    static async create({ newItem }) {
+      const createdNewItem = await ItemModel.create( newItem );
+      return createdNewItem;
+    }
+  
+    static async checkItemId({ itemId }) {
+      const item = await ItemModel.findOne({ _id: ObjectId(itemId) });
+      return item;
+    }
+  
+    static async findAll({ userId }) {
+      // ItemModel에서 주어진 userId에 해당하는 모든 아이템을 거래글 생성일 순으로 정렬해서 찾기
+      const items = await ItemModel.find({ userId }).sort({ createdAt: 'asc'});
+      const sortedItems = items.map(({...rest}) => [rest._doc].map(({userId, price, description, category, createdAt, updatedAt, __v, ...rest}) => rest)).flat();
+      return sortedItems;
+    }
+  
+    static async findByItemId({ itemId }) {
+      const item = await ItemModel.findOne({ _id: ObjectId(itemId) });
+      return item;
+    }
+  
+    static async update({ itemId, fieldToUpdate, newValue }) {
+      const filter = { _id: ObjectId(itemId) };
+      const update = { [fieldToUpdate]: newValue };
+      const option = { new : true };
+      const updatedItem = await ItemModel.findOneAndUpdate(filter, update, option);
+      return updatedItem;
+    }
+  
+    static async delete({ itemId }) {
+      const result = await ItemModel.findOneAndDelete({ _id: ObjectId(itemId) });
+      return result;
+    }
+  }
+  
+  export { Item };
+  
---
+++ back/src/db/schemas/item.js
@@ -0,0 +1,62 @@
+import { Schema, model } from "mongoose";
+
+const ItemSchema = new Schema(
+ {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+
+     email: {
+       type: String,
+       required: true,
+     },
+
+     title: {
+      type: String,
+      required: true,
+    },
+
+     description: {
+       type: String,
+       required: true,
+     },
+
+     category: {
+       type: String,
+       required: true,
+     },
+
+     like: {
+       type: Number,
+       default: 0
+     },
+
+	  price : {
+		  type: Number,
+      required: true,
+			default: 0
+		},
+
+    state : {
+		  type: String,
+      required: true,
+			default: "거래 가능"
+		},
+
+
+	itemsImgUrl: {
+      type: String,
+      required: false,
+    },
+
+   },
+   {
+     timestamps: true,
+   }
+ );
+ 
+const ItemModel = model("Item", ItemSchema);
+
+export { ItemModel };
