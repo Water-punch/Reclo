66e5c876c06c24bbca30804556f955d1104a4535
[AI9] 김수산 <psm77991133@gmail.com>
2023-11-28 17:23:34 +0900

    feat: wishlist/register

--- front/src/components/features/RegisterForm.jsx
+++ front/src/components/features/RegisterForm.jsx
@@ -1,62 +1,70 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import * as React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+import useLoginFormStore from "../../stores/useloginform";
 
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        RE:CLO
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+function LoginForm() {
+  const navigate = useNavigate();
 
-const defaultTheme = createTheme();
+  const { email, password, setEmail, setPassword, validateEmail } =
+    useLoginFormStore();
 
-export default function RegisterForm() {
   const handleSubmit = (event) => {
     event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+
+    if (!validateEmail(email)) {
+      console.log("유효하지 않은 이메일 형식입니다.");
+      return;
+    }
+
+    try {
+      console.log("Email:", email);
+      console.log("Password:", password);
+      // console.log({
+      //   email: email,
+      //   password: password,
+      // });
+    } catch (error) {
+      console.error("로그인 에러:", error);
+    }
   };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={createTheme()}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign up
+            회원가입
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -87,6 +95,8 @@ export default function RegisterForm() {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  value={email} // 추가
+                  onChange={(e) => setEmail(e.target.value)} // 추가
                 />
               </Grid>
               <Grid item xs={12}>
@@ -98,11 +108,15 @@ export default function RegisterForm() {
                   type="password"
                   id="password"
                   autoComplete="new-password"
+                  value={password} // 추가
+                  onChange={(e) => setPassword(e.target.value)} // 추가
                 />
               </Grid>
               <Grid item xs={12}>
                 <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  control={
+                    <Checkbox value="allowExtraEmails" color="primary" />
+                  }
                   label="I want to receive inspiration, marketing promotions and updates via email."
                 />
               </Grid>
@@ -113,19 +127,20 @@ export default function RegisterForm() {
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
-              Sign Up
+              회원가입
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="/login" variant="body2">
-                  Already have an account? Sign in
+                <Link href="/register" variant="body2">
+                  회원이 아니신가요? 가입하기
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
       </Container>
     </ThemeProvider>
   );
 }
+
+export default LoginForm;
--- front/src/stores/user.jsx
+++ front/src/stores/user.jsx
@@ -1,20 +1,19 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 //관리할 상태: 로그인, 포인트, 유저등급, 관심상품, 별명 ...
 const useUserStore = create((set) => ({
   login: false,
-  setLogin:() => set({ login: !login}),
-  nickName: '',
-  setNickName:(input) => set({ nickName: input }),
+  setLogin: () => set({ login: !login }),
+  nickName: "",
+  setNickName: (input) => set({ nickName: input }),
   rank: 0,
   //setLevel:
   point: 0,
   //setPoint:
-  birthDate: '',
+  birthDate: "",
   //setBirthdate:
   wishList: [],
-  //setLiked: 
+  //setLiked:
+}));
 
-}))
-
-export default useUserStore
+export default useUserStore;
