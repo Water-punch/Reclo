030693a91da2dccdaff2cf69d4ea1b510f1f338a
[AI9] 김진영 <a44428785@gmail.com>
2023-11-29 01:23:17 +0000

    Revert "Merge branch 'front/ss' into 'master'"
    This reverts merge request !6

--- front/src/components/pages/Items/index.jsx
+++
@@ -1,8 +0,0 @@
-export {default as EventPage} from './EventPage'
-
-export {default as ContentsPage} from './ContentsPage'
-export {default as ContentsAddPage} from './ContentsAddPage'
-export {default as ContentsDetailPage} from './ContentsDetailPage'
-
-
-
