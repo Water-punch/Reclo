45651abcbc7bfed2361021072eee8176aceb4e66
hyunjinb <hyunjinb394@gmail.com>
2023-12-02 00:36:30 +0900

    feat : wishItem user & detail 조회기능 추가

--- back/src/routers/wishItemRouter.js
+++ back/src/routers/wishItemRouter.js
@@ -5,8 +5,11 @@ const wishItemController = require('../controllers/wishItemController');
 
 const wishItemRouter = Router();
 
-// 관심상품 조회
-wishItemRouter.get('/item/:itemId/likes', asyncHandler(login_required), wishItemController.getLikedItems);
+// 유저별 관심상품 조회
+wishItemRouter.get('/item/user-likes/:userId', asyncHandler(login_required), wishItemController.getLikedItems);
+
+// 관심상품별 상세내용 조회
+wishItemRouter.get('/item/like-detail/:itemId', asyncHandler(login_required), wishItemController.getLikedItemDetails);
 
 // 관심상품 추가
 wishItemRouter.put('/item/:itemId/likes', asyncHandler(login_required), wishItemController.putItemLikes);
