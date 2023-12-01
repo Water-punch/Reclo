5d7db409519d55e38ecfaff5357d3ee619cecb68
hyunjinb <hyunjinb394@gmail.com>
2023-12-02 00:36:30 +0900

    feat : wishItem user & detail 조회기능 추가

--- back/src/db/models/WishItem.js
+++ back/src/db/models/WishItem.js
@@ -2,30 +2,28 @@ import { wishItemModel } from '../schemas/wishItem';
 const ObjectId = require('mongoose').Types.ObjectId;
 
 class wishItem {
-  // 아이템 아이디로 아이템 상품 찾기
-  //   static async findByItemId({ itemId }) {
-  //     const item = await wishItemModel.findOne({ $and: [{ itemId }, { deleted: false }] });
-  //     return item;
-  //   }
+  // 유저 아이디로 관심상품 찾기
+  static async findLikeitems({ userId }) {
+    const userLikeditems = await wishItemModel.findOne({ userId });
+    return userLikeditems;
+  }
 
-  // 좋아요 -> 관심상품 등록
+  // 관심상품 상세내용 조회
+  static async findLikeitemDetails({ itemId }) {
+    const item = await wishItemModel.findOne({ itemId }).populate('Item');
+    return item;
+  }
+
+  // Like -> 유저 관심상품 등록
   static async addLike({ newItem }) {
     const createNewWishItem = await wishItemModel.create(newItem);
     return createNewWishItem;
   }
 
-  // 좋아요 삭제 -> 관심상품 해제
+  // Dislike -> 유저 관심상품 해제
   static async deleteLike({ deleteItem }) {
     const deleteWishItem = await wishItemModel.deleteOne(deleteItem);
     return deleteWishItem;
-    //     const filter = { itemId: itemId,like: { $gt: 0 }  };
-    //     const update = {
-    //       $inc: { like: -1 },
-    //     };
-    //     const option = { returnOriginal: false };
-
-    //     const DeleteLike = await wishItemModel.findOneAndUpdate(filter, update, option);
-    //     return DeleteLike;
   }
 }
 
