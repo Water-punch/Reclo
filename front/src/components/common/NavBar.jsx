9ddaf3f231d125bc022ce3c34d6ee26b2b49b528
hiddenbebb <cbj4133@naver.com>
2023-12-03 04:57:17 +0900

    Updated: Navbar 수정 및 header 수정

--- front/src/components/common/NavBar.jsx
+++ front/src/components/common/NavBar.jsx
@@ -1,47 +1,65 @@
-import { useNavigate } from "react-router-dom"
-import * as React from 'react';
-import {Stack, Button, Typography} from '@mui/material'
+import { useNavigate } from "react-router-dom";
+import * as React from "react";
+import { Stack, Button, Typography } from "@mui/material";
+import "../../styles/NavBar.css";
 
 const NavBar = () => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
+  return (
+    <div className="navbar">
+      <Stack spacing={2} direction="row" my={3}>
+        <Typography
+          variant="h3"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          RE:CLO
+        </Typography>
+        <Button
+          variant="text"
+          color="success"
+          onClick={() => {
+            navigate("/intro");
+          }}
+          sx={{ flexShrink: 0 }}
+        >
+          서비스 소개
+        </Button>
+        <Button
+          variant="contained"
+          color="success"
+          sx={{ flexShrink: 0 }}
+          onClick={() => {
+            navigate("/contents");
+          }}
+        >
+          거래/나눔
+        </Button>
+        <Button
+          variant="outlined"
+          color="success"
+          sx={{ flexShrink: 0 }}
+          onClick={() => {
+            navigate("/event");
+          }}
+        >
+          이벤트
+        </Button>
+        <Button
+          variant="outlined"
+          color="success"
+          sx={{ flexShrink: 0 }}
+          onClick={() => {
+            navigate("/activity");
+          }}
+        >
+          💬거래진행
+        </Button>
+      </Stack>
+    </div>
+  );
+};
 
-    return (
-        <div className="navbar">
-          <Stack spacing={2} direction="row" my={3}>
-              <Typography variant='h3' onClick={()=>{navigate('/')}}>
-                RE:CLO
-              </Typography>
-              <Button 
-                variant="text" 
-                color="success"
-                onClick={() => {navigate('/intro')}}
-                sx={{ flexShrink: 0 }}>
-                서비스 소개
-              </Button>
-              <Button variant="contained"
-                color="success"
-                sx={{ flexShrink: 0 }}
-                onClick={() => {navigate('/contents')}}>
-                거래/나눔
-              </Button>
-              <Button 
-                variant="outlined" 
-                color="success"
-                sx={{ flexShrink: 0 }}
-                onClick={() => {navigate('/event')}}>
-                이벤트
-              </Button>
-              <Button 
-                variant="outlined" 
-                color="success"
-                sx={{ flexShrink: 0 }}
-                onClick={() => {navigate('/activity')}}>
-                💬거래진행
-              </Button>
-            </Stack>
-        </div>
-    )
-}
-
-export default NavBar
+export default NavBar;
