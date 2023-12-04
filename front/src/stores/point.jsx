efdbcd6687e310572b98b27c500b9fa147cbf32a
[AI9] 김수산 <psm77991133@gmail.com>
2023-12-04 17:24:35 +0000

    feat: point

---
+++ front/src/stores/point.jsx
@@ -0,0 +1,8 @@
+import { create } from "zustand";
+
+const usePointStore = create((set) => ({
+  usetData: {},
+  setUserData: (data) => set({ userData: data }),
+}));
+
+export default usePointStore;
