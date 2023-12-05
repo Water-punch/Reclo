63d24dfd94f42d49c81c662e64c0e205e57bf6e2
hyunjinb <hyunjinb394@gmail.com>
2023-12-05 17:36:33 +0900

    Remove : 머지 충돌 예방 프론트 폴더 삭제

--- front/src/components/pages/Items/index.jsx
+++
@@ -1,8 +0,0 @@
-export {default as EventPage} from './EventPage'
-
-export {default as ContentsPage} from './ContentsPage'
-export {default as ContentsWritePage} from './ContentsWritePage'
-export {default as ContentsDetailPage} from './ContentsDetailPage'
-
-
-
