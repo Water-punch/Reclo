1bc82c09817998e6d8f6c47050c954439bb47513
[AI9] 김수산 <psm77991133@gmail.com>
2023-12-07 05:15:22 +0900

    feat: 유저정보변경

---
+++ front/src/components/features/User/Changeinfo.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from 'react';
+import * as Api from '../../../api/api';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const ChangeInfo = () => {
+  const [newNickname, setNewNickname] = useState('');
+  const [profileImage, setProfileImage] = useState(null);
+  const [isChanging, setIsChanging] = useState(false);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+  const user = location.state.user;
+
+  useEffect(() => {
+    if (user) {
+      setNewNickname(user.nickname || '');
+    }
+  }, [user]);
+
+  const handleChangeUserInfo = async () => {
+    try {
+      if (!user) {
+        console.error('에러 발생: 유저 정보가 없습니다.');
+        return;
+      }
+
+      const response = await Api.put('user/current', {
+        user: {
+          _id: user._id,
+          nickname: newNickname,
+          profileImage,
+        },
+      });
+
+      setNewNickname(response.data.nickname);
+      setProfileImage(response.data.profileImage);
+
+      console.log('서버 응답:', response.data);
+
+      navigate('/userinfo', { state: { updatedUser: response.data } });
+    } catch (error) {
+      console.error('유저 정보 에러:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>User Change Page</h2>
+
+      {user ? (
+        <>
+          <p>현재 닉네임: {user.nickname}</p>
+
+          {isChanging ? (
+            <>
+              <input
+                type='text'
+                placeholder='새 닉네임'
+                value={newNickname}
+                onChange={(e) => setNewNickname(e.target.value)}
+              />
+              {/* userinfo에 이미지가 불러와지지 않는 중  */}
+              <input type='file' accept='image/*' onChange={(e) => setProfileImage(e.target.files[0])} />
+              <button onClick={handleChangeUserInfo}>저장</button>
+            </>
+          ) : (
+            <button onClick={() => setIsChanging(true)}>유저 정보 변경</button>
+          )}
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default ChangeInfo;
--- front/src/components/features/User/UserInfo.jsx
+++ front/src/components/features/User/UserInfo.jsx
@@ -1,36 +1,46 @@
-import { useNavigate } from 'react-router-dom';
-import '../../../styles/MyPage.css'
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import '../../../styles/MyPage.css';
 import UserPoint from './UserPoint';
 
 const UserInfo = ({ user }) => {
-  const navigate = useNavigate()
-  console.log(user)
-  console.log(user.email)
-  console.log(user.nickname)
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    return (
-      <div className="userinfoContainer">
-        <div className="mypage">
-          <h1>My Page</h1>
-          <div className="contentContainer">
-            <div className="profile"></div>
-            <div className="pointContainer">
-              <UserPoint />
-            </div>
+  const updatedUser = location.state?.updatedUser;
+
+  const displayUser = updatedUser || user;
+
+  console.log(displayUser);
+
+  return (
+    <div className='userinfoContainer'>
+      <div className='mypage'>
+        <h1>My Page</h1>
+        <div className='contentContainer'>
+          <div className='profile'>
+            {displayUser.profileImage ? (
+              <img src={displayUser.profileImage} alt='프로필 이미지' />
+            ) : (
+              <p>No profile image</p>
+            )}
           </div>
-          <div className="userbox">
-            <div className="userInfo">
-              <p>{user.email}</p>
-              <p>{user.nickname}</p>
-
-              <button className="change" onClick={() => navigate("/point")}>
-                point 조회
-              </button>
-            </div>
+          <div className='pointContainer'>
+            <UserPoint />
+          </div>
+        </div>
+        <div className='userbox'>
+          <div className='userInfo'>
+            <p>{displayUser.email}</p>
+            <p>{displayUser.nickname}</p>
+            <button className='change' onClick={() => navigate('/changeinfo', { state: { user: displayUser } })}>
+              유저 정보 변경
+            </button>
           </div>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default UserInfo
+export default UserInfo;
