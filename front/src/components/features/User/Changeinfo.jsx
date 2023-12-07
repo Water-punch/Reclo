5ba17e6443f2990bc241aa9f8ee20e618be70f9c
hiddenbebb <cbj4133@naver.com>
2023-12-07 22:29:40 +0900

    modified:   front/src/components/features/User/Changeinfo.jsx

--- front/src/components/features/User/Changeinfo.jsx
+++ front/src/components/features/User/Changeinfo.jsx
@@ -2,24 +2,6 @@ import React, { useState, useEffect } from 'react';
 import * as Api from '../../../api/api';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-const CenteredContainer = ({ children }) => (
-  <div
-    style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '50vh',
-      border: '1px solid black',
-      maxWidth: '800px',
-      margin: '30px auto 0',
-      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-    }}
-  >
-    {children}
-  </div>
-);
-
 const ChangeInfo = () => {
   const [newNickname, setNewNickname] = useState('');
   const [profileImage, setProfileImage] = useState(null);
@@ -55,45 +37,39 @@ const ChangeInfo = () => {
 
       console.log('서버 응답:', response.data);
 
-      navigate('/mypage', { state: { updatedUser: response.data } });
+      navigate('/userinfo', { state: { updatedUser: response.data } });
     } catch (error) {
       console.error('유저 정보 에러:', error);
     }
   };
 
   return (
-    <CenteredContainer>
-      <div>
-        <h2>닉네임, 프로필 사진 변경하기</h2>
+    <div>
+      <h2>User Change Page</h2>
 
-        {user ? (
-          <>
-            <p>현재 닉네임: {user.nickname}</p>
+      {user ? (
+        <>
+          <p>현재 닉네임: {user.nickname}</p>
 
-            {isChanging ? (
-              <>
-                <input
-                  type='text'
-                  placeholder='새 닉네임'
-                  value={newNickname}
-                  onChange={(e) => setNewNickname(e.target.value)}
-                />
-                <input type='file' accept='image/*' onChange={(e) => setProfileImage(e.target.files[0])} />
-                <button className='userchange-btn' onClick={handleChangeUserInfo}>
-                  저장
-                </button>
-              </>
-            ) : (
-              <button className='userchange-btn' onClick={() => setIsChanging(true)}>
-                유저 정보 변경
-              </button>
-            )}
-          </>
-        ) : (
-          <p>Loading...</p>
-        )}
-      </div>
-    </CenteredContainer>
+          {isChanging ? (
+            <>
+              <input
+                type='text'
+                placeholder='새 닉네임'
+                value={newNickname}
+                onChange={(e) => setNewNickname(e.target.value)}
+              />
+              <input type='file' accept='image/*' onChange={(e) => setProfileImage(e.target.files[0])} />
+              <button onClick={handleChangeUserInfo}>저장</button>
+            </>
+          ) : (
+            <button onClick={() => setIsChanging(true)}>유저 정보 변경</button>
+          )}
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
   );
 };
 
