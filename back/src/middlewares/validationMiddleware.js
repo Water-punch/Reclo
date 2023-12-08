d07be1e629ccb4a2dac71a57ee587825f0834df5
[AI9] 최원민 <wonmin98217@gmail.com>
2023-12-08 15:36:48 +0900

    feat :user,point  validation

--- back/src/middlewares/validationMiddleware.js
+++ back/src/middlewares/validationMiddleware.js
@@ -9,9 +9,9 @@ const validationCheck = (req, res, next) => {
 };
 
 const userValidation = [
-  body('email').notEmpty().withMessage('이메일을 입력해주세요'),
-  body('password').notEmpty().withMessage('패스워드를 입력해주세요'),
-  body('nickname').notEmpty().withMessage('닉네임을 입력해주세요'),
+  body('user.email').notEmpty().withMessage('이메일을 입력해주세요'),
+  body('user.password').notEmpty().withMessage('패스워드를 입력해주세요'),
+  body('user.nickname').notEmpty().withMessage('닉네임을 입력해주세요'),
   validationCheck,
 ];
 
@@ -21,4 +21,11 @@ const userLoginValidation = [
   validationCheck,
 ];
 
-export { userValidation, userLoginValidation };
+const pointValidation = [
+  body('pointdetails.userId').notEmpty().withMessage('userId가 필요합니다.'),
+  body('pointdetails.itemId').notEmpty().withMessage('itemId가 필요합니다.'),
+  body('pointdetails.point').notEmpty().withMessage('point 수치를 입력해 주세요'),
+  validationCheck,
+];
+
+export { userValidation, userLoginValidation, pointValidation };
